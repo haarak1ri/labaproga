6bd576dd7ea38319c55f86b385e69c24ae874616
haarak1ri <harak1richit@gmail.com>
2025-09-30 22:36:51 +0700

    Убрал цао вывод]

--- main.cpp
+++ main.cpp
@@ -4,27 +4,7 @@
 using namespace std;
 
 int main() {
-    // Конструктор с параметром
-    //MyString s1("English is");
 
-    // Конструктор по умолчанию + изменение строки
-   // MyString s2;
-    //s2.setString(" the best language");
-
-    // Объединение строк
-    //MyString s3 = s1.concat(s2);
-
-    // Вывод через c_str()
-    //cout << "s1: " << s1.c_str() << "\n";
-   // cout << "s2: " << s2.c_str() << "\n";
-   // cout << "s3: " << s3.c_str() << "\n";
-
-    // Поиск подстроки
-    //cout << "Substring 'best' found in s3 at position: "
-         //<< s3.find("best") << "\n";
-
-    // Статический метод
-   // cout << "Object count: " << MyString::getObjectCount() << "\n";
 
     return 0;
 }
